9a4f9e3920e6159310ed86809221f40218ec943d
Oliver Streuli <Oliver.Streuli@digitecgalaxus.ch>
2021-07-27 11:18:40 +0200

    t MoveAndTurnRoverWithMultipleInstructions

--- CSharp/MarsRoverTest/MarsRoverTest/RoverTest.cs
+++ CSharp/MarsRoverTest/MarsRoverTest/RoverTest.cs
@@ -13,12 +13,12 @@ namespace MarsRover
             var rover = new Rover(startDirection, positionX: startPosition[0], positionY: startPosition[1]);
 
             rover.Go(instructions: "f");
-            
+
             Assert.That(rover.Direction, Is.EqualTo(startDirection));
             Assert.That(rover.PositionX, Is.EqualTo(endPosition[0]));
             Assert.That(rover.PositionY, Is.EqualTo(endPosition[1]));
         }
-        
+
         [TestCase('N', new[] {5, 5}, new[] {5, 4})]
         [TestCase('S', new[] {5, 5}, new[] {5, 6})]
         [TestCase('E', new[] {5, 5}, new[] {4, 5})]
@@ -28,12 +28,12 @@ namespace MarsRover
             var rover = new Rover(startDirection, positionX: startPosition[0], positionY: startPosition[1]);
 
             rover.Go(instructions: "b");
-            
+
             Assert.That(rover.Direction, Is.EqualTo(startDirection));
             Assert.That(rover.PositionX, Is.EqualTo(endPosition[0]));
             Assert.That(rover.PositionY, Is.EqualTo(endPosition[1]));
         }
-        
+
         [TestCase('N', 'W')]
         [TestCase('W', 'S')]
         [TestCase('S', 'E')]
@@ -43,12 +43,12 @@ namespace MarsRover
             var rover = new Rover(startDirection, positionX: 13, positionY: 37);
 
             rover.Go(instructions: "l");
-            
+
             Assert.That(rover.Direction, Is.EqualTo(endDirection));
             Assert.That(rover.PositionX, Is.EqualTo(13));
             Assert.That(rover.PositionY, Is.EqualTo(37));
         }
-        
+
         [TestCase('N', 'E')]
         [TestCase('E', 'S')]
         [TestCase('S', 'W')]
@@ -58,10 +58,23 @@ namespace MarsRover
             var rover = new Rover(startDirection, positionX: 6, positionY: 9);
 
             rover.Go(instructions: "r");
-            
+
             Assert.That(rover.Direction, Is.EqualTo(endDirection));
             Assert.That(rover.PositionX, Is.EqualTo(6));
             Assert.That(rover.PositionY, Is.EqualTo(9));
+        }
+
+        [Test]
+        public void MoveAndTurnRoverWithMultipleInstructions()
+        {
+            var rover = new Rover(direction: 'N', positionX: 5, positionY: 5);
+
+            // N/5/5 -[f]-> N/5/6 -[r]-> E/5/6 -[b]-> E/4/6 -[b]-> E/3/6 -[l]-> N/3/6 -[f]-> N/3/7 -[l]-> W/3/7
+            rover.Go("frbblfl");
+
+            Assert.That(rover.Direction, Is.EqualTo('W'));
+            Assert.That(rover.PositionX, Is.EqualTo(3));
+            Assert.That(rover.PositionY, Is.EqualTo(7));
         }
     }
 }
