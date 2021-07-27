a29aad47c9db79216e60c38cf875f58e11b4f1ff
Oliver Streuli <Oliver.Streuli@digitecgalaxus.ch>
2021-07-27 11:00:35 +0200

    t MoveRoverBackwardWhenFacingGivenDirection

--- CSharp/MarsRoverTest/MarsRoverTest/RoverTest.cs
+++ CSharp/MarsRoverTest/MarsRoverTest/RoverTest.cs
@@ -10,9 +10,24 @@ namespace MarsRover
         [TestCase('W', new[] {5, 5}, new[] {4, 5})]
         public void MoveRoverForwardWhenFacingGivenDirection(char startDirection, int[] startPosition, int[] endPosition)
         {
-            var rover = new Rover(startDirection, startPosition[0], startPosition[1]);
+            var rover = new Rover(startDirection, positionX: startPosition[0], positionY: startPosition[1]);
 
             rover.Move("f");
+            
+            Assert.That(rover.Direction, Is.EqualTo(startDirection));
+            Assert.That(rover.PositionX, Is.EqualTo(endPosition[0]));
+            Assert.That(rover.PositionY, Is.EqualTo(endPosition[1]));
+        }
+        
+        [TestCase('N', new[] {5, 5}, new[] {5, 4})]
+        [TestCase('S', new[] {5, 5}, new[] {5, 6})]
+        [TestCase('E', new[] {5, 5}, new[] {4, 5})]
+        [TestCase('W', new[] {5, 5}, new[] {6, 5})]
+        public void MoveRoverBackwardWhenFacingGivenDirection(char startDirection, int[] startPosition, int[] endPosition)
+        {
+            var rover = new Rover(startDirection, positionX: startPosition[0], positionY: startPosition[1]);
+
+            rover.Move("b");
             
             Assert.That(rover.Direction, Is.EqualTo(startDirection));
             Assert.That(rover.PositionX, Is.EqualTo(endPosition[0]));
