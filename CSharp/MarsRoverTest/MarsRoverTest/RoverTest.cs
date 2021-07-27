b82d042483d03c2dccb064b2f622be18ef835587
Oliver S <r3zn1k@bluewin.ch>
2021-07-27 09:39:12 +0200

    Initial commit

---
+++ CSharp/MarsRoverTest/MarsRoverTest/RoverTest.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace MarsRover
+{
+    public class RoverTest
+    {
+        [Test]
+        public void Test1()
+        {
+            var marsRover = new Rover();
+            Assert.Pass();
+        }
+    }
+}
