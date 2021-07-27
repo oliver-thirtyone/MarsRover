c43b9872f60b159346f7a3bb84aa5df8881c9940
Oliver Streuli <Oliver.Streuli@digitecgalaxus.ch>
2021-07-27 10:53:34 +0200

    t move forward facing East

--- CSharp/MarsRoverTest/MarsRoverTest/RoverTest.cs
+++ CSharp/MarsRoverTest/MarsRoverTest/RoverTest.cs
@@ -6,6 +6,7 @@ namespace MarsRover
     {
         [TestCase('N', new[] {5, 5}, new[] {5, 6})]
         [TestCase('S', new[] {5, 5}, new[] {5, 4})]
+        [TestCase('E', new[] {5, 5}, new[] {6, 5})]
         public void MoveRoverForwardWhenFacingGivenDirection(char startDirection, int[] startPosition, int[] endPosition)
         {
             var rover = new Rover(startDirection, startPosition[0], startPosition[1]);
