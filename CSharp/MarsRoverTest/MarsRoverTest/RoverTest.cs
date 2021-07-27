8d241d600547120de4e8a20ac0074a821464c3ea
Oliver Streuli <Oliver.Streuli@digitecgalaxus.ch>
2021-07-27 10:56:25 +0200

    t west

--- CSharp/MarsRoverTest/MarsRoverTest/RoverTest.cs
+++ CSharp/MarsRoverTest/MarsRoverTest/RoverTest.cs
@@ -7,6 +7,7 @@ namespace MarsRover
         [TestCase('N', new[] {5, 5}, new[] {5, 6})]
         [TestCase('S', new[] {5, 5}, new[] {5, 4})]
         [TestCase('E', new[] {5, 5}, new[] {6, 5})]
+        [TestCase('W', new[] {5, 5}, new[] {4, 5})]
         public void MoveRoverForwardWhenFacingGivenDirection(char startDirection, int[] startPosition, int[] endPosition)
         {
             var rover = new Rover(startDirection, startPosition[0], startPosition[1]);
